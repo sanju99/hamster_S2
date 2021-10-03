fec1d1dcb77a967349f7170891ce11cea9dcb230
sanju99 <32857277+sanju99@users.noreply.github.com>
2021-10-03 01:13:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -92,100 +92,96 @@ dash1 = pn.Row(pn.Column(pn.layout.VSpacer(), ag_select, pn.Spacer(height=50), i
                pn.Spacer(width=40),
                ag_strip_plot)
 
-# @pn.depends(ab_fcr_select.param.value)
-# def ig_fcr_strip_plot(ig_or_fcr=df_plot.Ig_FcR.values[0]):
+@pn.depends(ab_fcr_select.param.value)
+def ig_fcr_strip_plot(ig_or_fcr=df_plot.Ig_FcR.values[0]):
     
-#     df_small = df_plot.loc[(df_plot.Ig_FcR == ig_or_fcr), :]
+    df_small = df_plot.loc[(df_plot.Ig_FcR == ig_or_fcr), :]
     
-#     if "Ig" in ig_or_fcr:
-#         title = f"Antigen-Specific {ig_or_fcr} Titers"
-#     else:
-#         title = f"{ig_or_fcr} Binding Across Antigens"
+    if "Ig" in ig_or_fcr:
+        title = f"Antigen-Specific {ig_or_fcr} Titers"
+    else:
+        title = f"{ig_or_fcr} Binding Across Antigens"
     
-#     strip = hv.Scatter(
-#                 data=df_small,
-#                 kdims=['Ag'],
-#                 vdims=["value", "Treatment", "Sample"],
-#             ).opts(
-#                 color='Treatment',
-#                 line_color="black",
-#                 alpha=0.8,
-#                 jitter=0.3,
-#                 size=9,
-#                 xlabel="",
-#                 ylabel='Median Fluorescence',
-#                 title=title,
-#                 logy=True,
-#                 width=900,
-#                 height=450,
-#                 legend_position="bottom",
-#                 tools=[bokeh.models.HoverTool(tooltips=[('Sample', '@Sample'), ('Fluor', '@value{int}')])],
-#                 cmap=['#1f77b4', 'darkorange', 'green'],
-#                 fontsize={'labels': 11, 'xticks': 10, 'yticks': 10}
-#             )
+    strip = hv.Scatter(
+                data=df_small,
+                kdims=['Ag'],
+                vdims=["value", "Treatment", "Sample"],
+            ).opts(
+                color='Treatment',
+                line_color="black",
+                alpha=0.8,
+                jitter=0.3,
+                size=9,
+                xlabel="",
+                ylabel='Median Fluorescence',
+                title=title,
+                logy=True,
+                width=900,
+                height=450,
+                legend_position="bottom",
+                tools=[bokeh.models.HoverTool(tooltips=[('Sample', '@Sample'), ('Fluor', '@value{int}')])],
+                cmap=['#1f77b4', 'darkorange', 'green'],
+                fontsize={'labels': 11, 'xticks': 10, 'yticks': 10}
+            )
     
-#     return strip
+    return strip
 
-# dash2 = pn.Column(ab_fcr_select, pn.Spacer(height=30), ig_fcr_strip_plot)
+dash2 = pn.Column(ab_fcr_select, pn.Spacer(height=30), ig_fcr_strip_plot)
 
 
-# def zscore_heatmap(df):
+def zscore_heatmap(df):
     
-#     df_zscore = df.select_dtypes(include=np.number).apply(st.zscore)
-#     df_zscore["Treatment"] = df.Treatment
-#     df_zscore["Sample"] = df.Sample
+    df_zscore = df.select_dtypes(include=np.number).apply(st.zscore)
+    df_zscore["Treatment"] = df.Treatment
+    df_zscore["Sample"] = df.Sample
 
-#     df_zscore.sort_values(by="Treatment", inplace=True)
+    df_zscore.sort_values(by="Treatment", inplace=True)
 
-#     df_hm = df_zscore.melt(id_vars=["Sample", "Treatment"])
-#     source = bokeh.models.ColumnDataSource(df_hm)
+    df_hm = df_zscore.melt(id_vars=["Sample", "Treatment"])
+    source = bokeh.models.ColumnDataSource(df_hm)
 
-#     color_num_max = np.max([abs(df_hm.value.min()), abs(df_hm.value.max())])
+    color_num_max = np.max([abs(df_hm.value.min()), abs(df_hm.value.max())])
 
-#     colors = list(sns.diverging_palette(220, 20).as_hex())
-#     mapper = LinearColorMapper(palette=colors, 
-#                                low=-color_num_max,
-#                                high=color_num_max)
+    colors = list(sns.diverging_palette(220, 20).as_hex())
+    mapper = LinearColorMapper(palette=colors, 
+                               low=-color_num_max,
+                               high=color_num_max)
 
-#     p = bokeh.plotting.figure(width=1350, 
-#                               height=600, 
-#                               # title="Hamster S2 Z-Score Data",
-#                               x_range=list(df_hm.variable.unique()), 
-#                               y_range=list(df_hm.Sample.unique())[::-1],
-#                               toolbar_location=None, x_axis_location="above",
-#                               tools=[bokeh.models.HoverTool(tooltips=[('Group', '@Treatment'), ('Z-Score', '@value{0.000}'), ('', '@variable')])])
+    p = bokeh.plotting.figure(width=1350, 
+                              height=600, 
+                              # title="Hamster S2 Z-Score Data",
+                              x_range=list(df_hm.variable.unique()), 
+                              y_range=list(df_hm.Sample.unique())[::-1],
+                              toolbar_location=None, x_axis_location="above",
+                              tools=[bokeh.models.HoverTool(tooltips=[('Group', '@Treatment'), ('Z-Score', '@value{0.000}'), ('', '@variable')])])
 
-#     p.rect(x="variable", y="Sample", width=1, height=1, source=source,
-#            line_color="white", 
-#            fill_color=transform('value', mapper),
-#           )
+    p.rect(x="variable", y="Sample", width=1, height=1, source=source,
+           line_color="white", 
+           fill_color=transform('value', mapper),
+          )
 
-#     color_bar = ColorBar(color_mapper=mapper,
-#                          ticker=BasicTicker(desired_num_ticks=len(colors)),
-#                         )
+    color_bar = ColorBar(color_mapper=mapper,
+                         ticker=BasicTicker(desired_num_ticks=len(colors)),
+                        )
 
-#     p.add_layout(color_bar, 'right')
+    p.add_layout(color_bar, 'right')
 
-#     p.axis.axis_line_color = None
-#     p.axis.major_tick_line_color = None
+    p.axis.axis_line_color = None
+    p.axis.major_tick_line_color = None
 
-#     p.xaxis.major_label_text_font_size = "10px"
-#     p.yaxis.major_label_text_font_size = "12px"
+    p.xaxis.major_label_text_font_size = "10px"
+    p.yaxis.major_label_text_font_size = "12px"
 
-#     p.axis.major_label_standoff = 0
-#     p.xaxis.major_label_orientation = 1.0
+    p.axis.major_label_standoff = 0
+    p.xaxis.major_label_orientation = 1.0
     
-#     return p
+    return p
 
-# dash3 = zscore_heatmap(df)
+dash3 = zscore_heatmap(df)
 
-# tab1 = pn.Row(pn.layout.HSpacer(), pn.Column(dash1, pn.Spacer(height=20), dash2), pn.layout.HSpacer())
-# tab2 = pn.Row(pn.layout.HSpacer(), dash3, pn.layout.HSpacer())
+tab1 = pn.Row(pn.layout.HSpacer(), pn.Column(dash1, pn.Spacer(height=20), dash2), pn.layout.HSpacer())
+tab2 = pn.Row(pn.layout.HSpacer(), dash3, pn.layout.HSpacer())
 
-# dashboard = pn.Tabs(('Strip Plots', tab1), ("Z-Score Heatmap", tab2))
-
-# dashboard = pn.Row(pn.layout.HSpacer(), dash3, pn.layout.HSpacer())
-
-dashboard = pn.Column("Please fucking work!", dash1)
+dashboard = pn.Tabs(('Strip Plots', tab1), ("Z-Score Heatmap", tab2))
 
 dashboard.servable()
