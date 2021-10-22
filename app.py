2969bfb72314b86ee4249161a9db279deb0704b5
Sanjana <sanjukul99@gmail.com>
2021-10-21 23:13:59 -0400

    Significant edits Oct 21

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 import scipy.stats as st
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 import bokeh.io
 import bokeh.plotting
@@ -19,10 +20,14 @@ pn.extension()
 
 from bokeh.models import BasicTicker, ColorBar, LinearColorMapper, PrintfTickFormatter
 from bokeh.transform import transform
+from sklearn.cross_decomposition import PLSRegression
+
+from utils import *
 
 
 # read in and process the dataframe
-df = pd.read_csv("Halfmann_P1.csv")
+#df = pd.read_csv("Halfmann_P1.csv")
+df = pd.read_csv("Halfmann_P1_bgSubtracted.csv")
 
 # remove empty columns
 df = df[df.columns[~df.columns.str.contains("Unnamed")]]
@@ -38,23 +43,23 @@ ab_fcr_lst = [name.split("_")[1] for name in luminex_cols]
 ag_select = pn.widgets.Select(name='Select Antigen', options=list(np.unique(ag_lst)))
 ab_fcr_select = pn.widgets.Select(name='Select Ig or FcR', options=list(np.unique(ab_fcr_lst)))
 ig_fcr_choose = pn.widgets.RadioButtonGroup(options=['Ig Titer', 'FcR Binding'], button_type='primary')
+box_plot_toggle = pn.widgets.Toggle(name="Show Box Plot", button_type="success")
 
 # split dataframe into Luminex and functional data
 
 df_luminex = pd.concat([df.loc[:, df.columns.str.contains("_")], df[["Immunization", "Challenge", "Sample"]]], axis=1)
-df_func = df.loc[:, ~df.columns.str.contains("_")]
+df_func = pd.concat([df.loc[:, df.columns.str.contains("AD")], df[["Immunization", "Challenge", "Sample"]]], axis=1)
 df_challenge = pd.concat([df.loc[:, df.columns.str.contains("pfu")], df[["Immunization", "Challenge", "Sample"]]], axis=1)
 
 # make the data tidy for plotting
 
 df_luminex_plot = df_luminex.melt(id_vars=["Immunization", "Challenge", "Sample"])
 df_func_plot = df_func.melt(id_vars=["Immunization", "Challenge", "Sample"])
-df_challenge_plot = df_func.melt(id_vars=["Immunization", "Challenge", "Sample"])
+df_challenge_plot = df_challenge.melt(id_vars=["Immunization", "Challenge", "Sample"])
 
 antigens, igs_fcrs = list(zip(*df_luminex_plot.variable.str.split("_")))
 df_luminex_plot["Ag"] = antigens
 df_luminex_plot["Ig_FcR"] = igs_fcrs
-
 
 opts.defaults(
     opts.Scatter(
@@ -71,10 +76,21 @@ opts.defaults(
     ),
 )
 
+assay_abbreviation_dict = {"ADCD": "Complement Deposition",
+                           "mADCP": "J774A Monocyte Phagocytosis",
+                           "hADCP": "THP-1 Monocyte Phagocytosis",
+                           "hADNP": "Human Neutrophil Phagocytosis"}
+
+compartment_abbreviation_dict = {"Lung (log10 pfu/g)": "Lung Viral Load",
+                                 "NT (log10 pfu/g)": "Nasal Turbinate Viral Load"}
+
 
 @pn.depends(ag_select.param.value,
-            ig_fcr_choose.param.value)
-def ag_strip_plot(antigen=df_luminex_plot.Ag.values[0], ig_or_fcr="Ig Titer"):
+            ig_fcr_choose.param.value,
+            box_plot_toggle.param.value)
+def ag_strip_plot(antigen=df_luminex_plot.Ag.values[0], 
+                  ig_or_fcr="Ig Titer",
+                  show_box_plot=False):
     
     if ig_or_fcr == "Ig Titer":
         df_small = df_luminex_plot.loc[(df_luminex_plot.Ag == antigen) & (df_luminex_plot.Ig_FcR.str.contains("Ig")), :]
@@ -82,28 +98,66 @@ def ag_strip_plot(antigen=df_luminex_plot.Ag.values[0], ig_or_fcr="Ig Titer"):
     else:
         df_small = df_luminex_plot.loc[(df_luminex_plot.Ag == antigen) & (df_luminex_plot.Ig_FcR.str.contains("Fc")), :]
         title = f"{antigen}-specific Antibodies to FcRs"
+
+    if show_box_plot:
         
-    strip = hv.Scatter(
-                data=df_small,
-                kdims=['Ig_FcR'],
-                vdims=["value", "Immunization", "Sample"],
-            ).opts(
-                color='Immunization',
-                title=title,
-                width=750,
-                height=400,
-                fontsize={'labels': 11, 'xticks': 10, 'yticks': 10}
-            )
+        # make box plot
+        box = hv.BoxWhisker(
+            data=df_small,
+            kdims=["Ig_FcR", "Immunization"],
+            vdims='value',
+        ).opts(
+            ylabel="MFI",
+            whisker_color='gray',
+            box_line_color="gray",
+            box_fill_color="white",
+            height=500,
+            width=800,
+            outlier_alpha=0,
+            logy=True,
+            title = title,
+            fontsize={'labels': 11, 'xticks': 10, 'yticks': 10}
+        )
+
+        # extract bokeh object
+        p = hv.render(box)
     
-    p = hv.render(strip)
-    p.add_layout(p.legend[0], 'right')
+        iqplot.strip(p=p,
+                    data=df_small, 
+                    q="value", 
+                    cats=["Ig_FcR", "Immunization"], 
+                    q_axis='y', 
+                    color_column="Immunization",
+                    jitter=True,
+                    marker_kwargs={"size": 8, "line_color": "black"},
+                    jitter_kwargs={"width": 0.25},
+                    tooltips=[('Sample', '@Sample'), ("Fluor", '@value')]
+                    )
+        
+    else:
+        
+        p = iqplot.strip(data=df_small, 
+                    q="value", 
+                    cats=["Ig_FcR", "Immunization"], 
+                    q_axis='y', 
+                    color_column="Immunization",
+                    jitter=True,
+                    marker_kwargs={"size": 8, "line_color": "black"},
+                    jitter_kwargs={"width": 0.25},
+                    title=title,
+                    height=500,
+                    width=800,
+                    y_axis_type="log",
+                    y_axis_label="MFI",
+                    tooltips=[('Sample', '@Sample'), ("Fluor", '@value')]
+                )
+
     p.toolbar_location = "above"
-    p.legend.title = "Immunization"
     
     return p
 
 
-dash1 = pn.Row(pn.Column(pn.layout.VSpacer(), ag_select, pn.Spacer(height=50), ig_fcr_choose, pn.layout.VSpacer()), 
+dash1 = pn.Row(pn.Column(pn.layout.VSpacer(), ag_select, pn.Spacer(height=30), ig_fcr_choose, pn.Spacer(height=30), box_plot_toggle, pn.layout.VSpacer()), 
                pn.Spacer(width=40),
                ag_strip_plot)
 
@@ -135,158 +189,51 @@ def ig_fcr_strip_plot(ig_or_fcr=df_luminex_plot.Ig_FcR.values[0]):
     
     return p
 
+
+# Interactive plots for data exploration
 dash2 = pn.Column(ab_fcr_select, pn.Spacer(height=30), ig_fcr_strip_plot)
-
-
-def func_stripbox(df):
-            
-    assay_abbreviation_dict = {"ADCD": "Complement Deposition",
-                               "mADCP": "J774A Monocyte Phagocytosis",
-                               "hADCP": "THP-1 Monocyte Phagocytosis",
-                               "hADNP": "Human Neutrophil Phagocytosis"}
-
-    
-    plots = []
-    
-    for func_assay in assay_abbreviation_dict.keys():
-
-        df_small = df.loc[df.variable == func_assay]
-
-        p = iqplot.stripbox(data=df_small, 
-                            q="value", 
-                            cats="Immunization", 
-                            q_axis='y', 
-                            jitter=True,
-                            marker_kwargs={"size": 8, "line_color": "black"},
-                            jitter_kwargs={'width': 0.25},
-                            tooltips=[('Sample', '@Sample')],
-                            toolbar_location="right",
-                            height=400,
-                            width=500,
-                            y_axis_type="log",
-                            x_axis_label="Group",
-                            y_axis_label="MFI",
-                            title=assay_abbreviation_dict[func_assay],
-                           )
-
-        plots.append(p)
-        
-    return bokeh.layouts.gridplot(plots, ncols=2, merge_tools=False)
-
-
-abbreviation_dict = {"Lung (log10 pfu/g)": "Lung Viral Load",
-                     "NT (log10 pfu/g)": "Nasal Turbinate Viral Load"}
-
-def viral_load_stripbox(df):
-
-    plots = []
-
-    for location in abbreviation_dict.keys():
-
-        df_small = df.loc[df.variable == location]
-        
-        # make box plot
-        box = hv.BoxWhisker(
-            data=df_small,
-            kdims=['Immunization', 'Challenge'],
-            vdims='value',
-        ).opts(
-            ylabel="Log10 (PFU/g)",
-            whisker_color='gray',
-            box_line_color="gray",
-            box_fill_color="white",
-            height=500,
-            width=600,
-            outlier_alpha=0,
-            title = abbreviation_dict[location],
-            fontsize={'labels': 11, 'xticks': 10, 'yticks': 10}
-        )
-
-        # extract bokeh object
-        p = hv.render(box)
-        p.toolbar_location = "above"
-                
-        plots.append(iqplot.strip(p=p,
-                            data=df_small, 
-                            q="value", 
-                            cats=["Immunization", "Challenge"], 
-                            q_axis='y', 
-                            jitter=True,
-                            marker_kwargs={"size": 8, "line_color": "black"},
-                            jitter_kwargs={"width": 0.25},
-                            toolbar_location="above",
-                            height=450,
-                            width=550,
-                            y_axis_type="log",
-                            x_axis_label="Challenge Variant",
-                            y_axis_label="MFI",
-                            title=abbreviation_dict[location],
-                            tooltips=[('Sample', '@Sample')]
-                           )
-                    )
-        
-    return bokeh.layouts.gridplot(plots, ncols=2, merge_tools=False)
-
-
-def zscore_heatmap(df):
-    
-    df_zscore = df.select_dtypes(include=np.number).apply(st.zscore)
-    df_zscore["Immunization"] = df.Immunization
-    df_zscore["Sample"] = df.Sample
-
-    df_zscore.sort_values(by="Immunization", inplace=True)
-
-    df_hm = df_zscore.melt(id_vars=["Sample", "Immunization"])
-    source = bokeh.models.ColumnDataSource(df_hm)
-
-    color_num_max = np.max([abs(df_hm.value.min()), abs(df_hm.value.max())])
-
-    colors = list(sns.diverging_palette(220, 20).as_hex())
-    mapper = LinearColorMapper(palette=colors, 
-                               low=-color_num_max,
-                               high=color_num_max)
-
-    p = bokeh.plotting.figure(width=1400, 
-                              height=600, 
-                              # title="Hamster S2 Z-Score Data",
-                              x_range=list(df_hm.variable.unique()), 
-                              y_range=list(df_hm.Sample.unique())[::-1],
-                              toolbar_location=None, x_axis_location="above",
-                              tools=[bokeh.models.HoverTool(tooltips=[('Immunization', '@Immunization'), ('Z-Score', '@value{0.000}'), ('', '@variable')])])
-
-    p.rect(x="variable", y="Sample", width=1, height=1, source=source,
-           line_color="white", 
-           fill_color=transform('value', mapper),
-          )
-
-    color_bar = ColorBar(color_mapper=mapper,
-                         ticker=BasicTicker(desired_num_ticks=len(colors)),
-                        )
-
-    p.add_layout(color_bar, 'right')
-
-    p.axis.axis_line_color = None
-    p.axis.major_tick_line_color = None
-
-    p.xaxis.major_label_text_font_size = "10px"
-    p.yaxis.major_label_text_font_size = "12px"
-
-    p.axis.major_label_standoff = 0
-    p.xaxis.major_label_orientation = 1.0
-    
-    return p
-
-
-dash3 = zscore_heatmap(df)
-
 tab1 = pn.Row(pn.layout.HSpacer(), pn.Column(dash1, pn.Spacer(height=20), dash2), pn.layout.HSpacer())
 
-tab2 = pn.Row(pn.layout.HSpacer(), func_stripbox(df_func_plot), pn.layout.HSpacer())
+# Functional assays
+tab2 = pn.Row(pn.layout.HSpacer(), stripbox(df_func_plot, ["Immunization"], assay_abbreviation_dict), pn.layout.HSpacer())
 
-tab3 = pn.Row(pn.layout.HSpacer(), viral_load_stripbox(df_challenge_plot), pn.layout.HSpacer())
+# Viral Loads
+tab3 = pn.Row(pn.layout.HSpacer(), stripbox(df_challenge_plot, ["Immunization", "Challenge"], compartment_abbreviation_dict), pn.layout.HSpacer())
 
-tab4 = pn.Row(pn.layout.HSpacer(), dash3, pn.layout.HSpacer())
+# Z Scores of all the data
+df_log = log_transform_data(df)
+tab4 = pn.Row(pn.layout.HSpacer(), zscore_heatmap(df_log), pn.layout.HSpacer())
 
-dashboard = pn.Tabs(('Luminex Plots', tab1), ("Functional Assays", tab2), ("Viral Loads", tab3), ("Z-Score Heatmap", tab4))
+# Correlations to identify features that might be significant predictors of viral load
+df_corr_drop = df_log[df_log.columns[~df_log.columns.str.contains("|".join(["pfu", "Ig"]))]]
+
+lung_correlation = df_corr_drop.corrwith(df_log["Lung (log10 pfu/g)"])
+NT_correlation = df_corr_drop.corrwith(df_log["NT (log10 pfu/g)"])
+
+lung_corr_df = pd.DataFrame({"Variable": lung_correlation.index, "Correlation": lung_correlation.values}).sort_values(by="Correlation")
+NT_corr_df = pd.DataFrame({"Variable": NT_correlation.index, "Correlation": NT_correlation.values}).sort_values(by="Correlation")
+
+# Linear model of the features computed above
+tab5 = pn.Row(pn.layout.HSpacer(),
+              pn.Column(pn.Row(
+                                pn.Column(pn.Spacer(height=25), correlation_plot(lung_corr_df, "Lung Viral Load Correlates")), 
+                                pls_regression(df_log, lung_corr_df, -2, 4, "Lung Viral Load Model")),
+                        pn.Row(
+                                pn.Column(pn.Spacer(height=25), correlation_plot(NT_corr_df, "Nasal Turbinate Viral Load Correlates")),
+                                pls_regression(df_log, NT_corr_df, -1, 4, "Nasal Turbinate Viral Load Model"))
+                       ),
+              pn.layout.HSpacer())
+
+# Flower plots
+col_names = ["Immunization", "Sample"] + list(df.columns[df.columns.str.contains("|".join(["S2", "AD"]))])
+flower_plots = polar_area_plot(df_log, col_names)
+tab6 = pn.Row(pn.layout.HSpacer(), flower_plots, pn.layout.HSpacer())
+
+dashboard = pn.Tabs(('Luminex Plots', tab1), 
+                    ("Functional Assays", tab2), 
+                    ("Viral Loads", tab3), 
+                    ("Z-Score Heatmap", tab4),
+                    ("Significant Features", tab5),
+                    ("Flower Plots", tab6))
 
 dashboard.servable()
---
+++ utils.py
@@ -0,0 +1,247 @@
+import numpy as np
+import pandas as pd
+
+import scipy.stats as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import bokeh.io
+import bokeh.plotting
+import bokeh.models
+
+import holoviews as hv
+hv.extension("bokeh")
+from holoviews import opts
+
+import iqplot
+
+import panel as pn
+pn.extension()
+
+from bokeh.models import BasicTicker, ColorBar, LinearColorMapper, PrintfTickFormatter
+from bokeh.transform import transform
+from sklearn.cross_decomposition import PLSRegression
+
+
+def log_transform_data(df):
+    '''
+    This function log-transforms the non-log-transformed data in the dataframe
+    '''
+    
+    return pd.concat([df[["Challenge", "Immunization", "Sample"]], np.log10(df.iloc[:, 3:-2]), df.iloc[:, -2:]], axis=1)
+
+
+
+def stripbox(df_plot, kdims_lst, abbrev_dict):
+    
+    plots = []
+    
+    for key in abbrev_dict.keys():
+
+        df_small = df_plot.loc[df_plot.variable == key]
+
+       # make box plot
+        box = hv.BoxWhisker(
+            data=df_small,
+            kdims=kdims_lst,
+            vdims='value',
+        ).opts(
+            ylabel="MFI",
+            whisker_color='gray',
+            box_line_color="gray",
+            box_fill_color="white",
+            height=500,
+            width=600,
+            outlier_alpha=0,
+            title = abbrev_dict[key],
+            fontsize={'labels': 11, 'xticks': 10, 'yticks': 10}
+        )
+
+        # extract bokeh object
+        p = hv.render(box)
+        p.toolbar_location = "above"
+                
+        plots.append(iqplot.strip(p=p,
+                            data=df_small, 
+                            q="value", 
+                            cats=kdims_lst, 
+                            q_axis='y', 
+                            jitter=True,
+                            marker_kwargs={"size": 8, "line_color": "black"},
+                            jitter_kwargs={"width": 0.25},
+                            height=450,
+                            width=550,
+                            y_axis_type="log",
+                            x_axis_label="Challenge Variant",
+                            y_axis_label="MFI",
+                            title=abbrev_dict[key],
+                            tooltips=[('Sample', '@Sample')],
+                           )
+                    )
+        
+    return bokeh.layouts.gridplot(plots, ncols=2, merge_tools=False)
+
+
+def polar_area_plot(df_log, col_names):
+    '''
+    Make a polar area (flower) plot for each hamster
+    '''
+
+    df_split = df_log[col_names].sort_values(by="Immunization")
+    new_ordering = ["Immunization", "Sample", "S2_IgG1", "S2_IgG2", "S2_IgA", "S2_IgM", 'S2_FcγRIIb', 'S2_FcγRIII', 'S2_FcγRIV', 'ADCD', 'mADCP', 'hADCP', 'hADNP']
+    df_split = df_split[new_ordering]
+    
+    titles = df_split.Sample.values
+    del df_split["Immunization"]
+    del df_split["Sample"]
+    
+    variables = [df_split.columns[i].split("_")[-1] if "_" in df_split.columns[i] else df_split.columns[i] for i in range(len(df_split.columns))]
+    
+    # Compute pie slices
+    N = len(df_split.columns)
+    
+    theta = np.linspace(0, 2 * np.pi, N, endpoint=False)
+    width = 2 * np.pi / N
+
+    colors = ["purple" if "AD" in variables[k] else "orange" for k in range(len(df_split.columns))]
+
+    sns.set_style("dark")
+
+    fig, ax = plt.subplots(4, 4, figsize=(18, 18), subplot_kw={'projection': 'polar'}, constrained_layout=True)
+    ax = ax.flatten()
+    
+    for i in range(len(df_split)):
+
+        radii = df_split.iloc[i, :].values
+        
+        ax[i].set_title(titles[i], pad=50, fontsize=16)
+        ax[i].tick_params(axis='x', pad=10)
+
+        ax[i].bar(theta, 
+               radii, 
+               width=width,
+               bottom=0, 
+               color=colors,
+               edgecolor="black")
+
+        ax[i].set_yticklabels([])
+        ax[i].set_ylim([0, int(np.ceil(np.max(np.max(df_split))))])
+
+        ax[i].set_xticks(theta)
+        ax[i].set_xticklabels(variables)        
+
+        ax[i].tick_params(axis='both', which='major', labelsize=12)
+
+    plt.close()
+    return pn.pane.Matplotlib(fig)
+
+
+def zscore_heatmap(df):
+    
+    df_zscore = df.select_dtypes(include=np.number).apply(st.zscore)
+    
+    # neat ordering of the x axis
+    heatmap_order = list(df_zscore.columns[df_zscore.columns.str.contains("IgG1")]) + \
+                    list(df_zscore.columns[df_zscore.columns.str.contains("IgG2")]) + \
+                    list(df_zscore.columns[df_zscore.columns.str.contains("IgA")]) + \
+                    list(df_zscore.columns[df_zscore.columns.str.contains("IgM")]) + \
+                    list(df_zscore.columns[df_zscore.columns.str.contains("FcγRIIb")]) + \
+                    list(df_zscore.columns[df_zscore.columns.str.contains("FcγRIII")]) + \
+                    list(df_zscore.columns[df_zscore.columns.str.contains("FcγRIV")]) + \
+                    list(df_zscore.columns[df_zscore.columns.str.contains("pfu")])
+    
+    df_zscore = pd.concat([df[["Challenge", "Immunization", "Sample"]], df_zscore[heatmap_order], ], axis=1)    
+    df_zscore.sort_values(by=["Immunization", "Challenge"], inplace=True)
+    
+    df_hm = df_zscore.melt(id_vars=["Sample", "Immunization", "Challenge"])
+    source = bokeh.models.ColumnDataSource(df_hm)
+
+    color_num_max = np.max([abs(df_hm.value.min()), abs(df_hm.value.max())])
+
+    colors = list(sns.diverging_palette(220, 20).as_hex())
+    mapper = LinearColorMapper(palette=colors, 
+                               low=-color_num_max,
+                               high=color_num_max)
+
+    p = bokeh.plotting.figure(width=1400, 
+                              height=600, 
+                              # title="Hamster S2 Z-Score Data",
+                              x_range=list(df_hm.variable.unique()), 
+                              y_range=list(df_hm.Sample.unique())[::-1],
+                              toolbar_location=None, x_axis_location="above",
+                              tools=[bokeh.models.HoverTool(tooltips=[('Immunization', '@Immunization'), 
+                                                                      ('Z-Score', '@value{0.000}'), 
+                                                                      ('Feature', '@variable'),
+                                                                      ("Challenge", "@Challenge")])])
+
+    p.rect(x="variable", y="Sample", width=1, height=1, source=source,
+           line_color="white", 
+           fill_color=transform('value', mapper),
+          )
+
+    color_bar = ColorBar(color_mapper=mapper,
+                         ticker=BasicTicker(desired_num_ticks=len(colors)),
+                        )
+
+    p.add_layout(color_bar, 'right')
+
+    p.axis.axis_line_color = None
+    p.axis.major_tick_line_color = None
+
+    p.xaxis.major_label_text_font_size = "10px"
+    p.yaxis.major_label_text_font_size = "12px"
+
+    p.axis.major_label_standoff = 0
+    p.xaxis.major_label_orientation = 1.0
+    
+    return p
+
+
+def correlation_plot(corr_df, title=""):
+    
+    corr_plot = hv.Bars(corr_df, 
+            kdims=['Variable'], 
+            vdims=["Correlation"]
+           ).opts(height=450,
+                  width=1000,
+                  ylim=(0, np.min(corr_df.Correlation)-0.05),
+                  xrotation=90,
+                  fill_color="#1f77b4",
+                  ylabel="Spearman Correlation",
+                  title=title,
+                  tools=[bokeh.models.HoverTool(tooltips=[('', '@Correlation{0.0000}'), ('', '@Variable')])],
+        )
+
+    p = hv.render(corr_plot)
+    p.toolbar_location = "above"
+    p.title.text_font_size = "16px"
+
+    return p
+
+
+def pls_regression(df_log, viral_load_corr, index, num_features=4, title=""):
+    
+    df_model = df_log.iloc[:, 3:-2]
+    
+    #X = df_model[viral_load_corr.iloc[:num_features, 0].values].values
+    X = df_model.values
+    y = df_log.iloc[:, index].values
+
+    pls2 = PLSRegression(n_components=num_features)
+    pls2.fit(X, y)
+    Y_pred = pls2.predict(X)
+    
+    pearson = st.pearsonr(Y_pred.flatten(), df_log.iloc[:, index].values)[0]
+    
+    df_plot = pd.DataFrame({"pred": Y_pred.flatten(), "actual": df_log.iloc[:, index].values})
+        
+    fig, ax = plt.subplots(figsize=(8, 7))
+    
+    sns.regplot(x="pred", y="actual", data=df_plot, ax=ax, ci=95)
+    ax.set_xlabel("Predicted", fontsize=12)
+    ax.set_ylabel("Actual", fontsize=12)
+    ax.set_title(f"{title}, Pearson Correlation = {round(pearson, 4)}", fontsize=16)
+    
+    plt.close()
+    
+    return pn.pane.Matplotlib(fig)
